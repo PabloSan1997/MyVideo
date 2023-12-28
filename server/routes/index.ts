8f3ff4c28b0b9cabe639622213b62d0d12b346b2
PabloSan1997 <jos_pab_997@hotmali.com>
2023-12-27 18:51:36 -0600

    Se agrego la configuracion para mandar archivos estaticos

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,12 @@
 import express, { type Express } from 'express';
 import { usuarioRouter } from './usuario.routes';
 import { videosRoutes } from './videos.routes';
+
+
 export function crearApi(app: Express): void {
 	const mainRoute = express.Router();
 	app.use('/api/v1', mainRoute);
 	mainRoute.use('/video', videosRoutes);
 	mainRoute.use('/user', usuarioRouter);
+	app.use('/', express.static('static/pagina'));
 }
